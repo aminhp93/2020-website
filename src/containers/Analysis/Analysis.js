576fc9a79abae10b5a04121070ff725c4d72d21d
Minh Pham <minhpham@Minhs-MacBook-Pro-2.local>
2020-03-15 11:29:29 +0700

    Add analysis

--- src/containers/Analysis/Analysis.js
+++ src/containers/Analysis/Analysis.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { List, Typography } from 'antd';
 
 import {
     setSymbol,
 } from '../../actions/stock';
 
+const data = [
+    {
+        'title': 'Level 1: Understand the detail of each row if financial reports'
+    },
+    {
+        'title': 'Level 2: Analyze the indicator (PE, EPS, ROE, ROA)'
+    },
+    {
+        'title': 'Level 3: Be aware of the financial fraud in financial report to be away from these stocks'
+    },
+    {
+        'title': 'Level 4: Point out the opportunities in short term investment',
+        'detail': [
+            'Nha may sap het khau hao',
+            'Loi nhuan dot bien tu hoan nhap du phong',
+            'Loi nhuan tu viec ban giao du an',
+            'Loi nhuan tu nhu cau tang cao dot bien',
+            'Loi nhuan tu tich tru hang ton kho gia re',
+            'Loi nhuan tu bien dong ty gia',
+        ]
+    },
+    {
+        'title': "Level 5: Big picture of business in real life",
+        'detail': [
+            'Tu phan tich bao cao tai chinh, danh gia: loi the canh tranh, vi the doanh nghiep',
+            'Danh gia phong cach quan tri, kinh doanh, rui ro, doi xu voi co dong.. cua ban lanh dao --> ben vung cua doanh nghiep',
+            'Suc khoe tai chinh trong tuong lai: ket qua kinh doanh, hieu qua kinh doanh, kha nang sinh loi dai han',
+            'Danh gia 3 nam, 5 nam, 10 nam',
+        ]
+    }
+];
 
 class Analysis extends React.Component {
     render() {
         return (
-            <div>
-                Analysis
+            <div className="Analysis">
+                <List
+                    header={<div>Analysis</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                            <div>
+                                <li>{item.title}</li>
+                                {
+                                    item.detail && item.detail.map(i => {
+                                        return <li>{i}</li>
+                                    })
+                                }
+                            </div>
+                        </List.Item>
+                    )}
+                />
+
             </div>
         )
     }
--- src/containers/Transaction/Transaction.js
+++ src/containers/Transaction/Transaction.js
@@ -1,10 +1,229 @@
 import React from 'react';
+import { List, Typography, Tabs } from 'antd';
+
 import ChartTV from '../ChartTV/ChartTV';
 
+const { TabPane } = Tabs;
+
+const data = [
+    'Racing car sprays burning fuel into crowd.',
+    'Japanese princess to wed commoner.',
+    'Australian walks 100km after outback crash.',
+    'Man charged over missing wedding girl.',
+    'Los Angeles battles huge wildfires.',
+];
 export default class Transaction extends React.Component {
+    renderSummaryTransaction = () => {
+        const dataSummaryTransaction = [
+            {
+                'title': 'Hiện tại',
+                'detail': '48.15 0.05|0.10%'
+            },
+            {
+                'title': 'Cao/Thấp',
+                'detail': '50.00 | 45.20'
+            },
+            {
+                'title': 'Khối lượng',
+                'detail': '2,919,140'
+            },
+            {
+                'title': 'Giá trị',
+                'detail': '136.99 tỷ'
+            },
+            {
+                'title': 'KLTB (10 ngày)',
+                'detail': '2,287,579'
+            }
+        ]
+        return (
+            <div>
+                <List
+                    header={<div>GIAO DỊCH</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={dataSummaryTransaction}
+                    renderItem={item => (
+                        <List.Item>
+                            <div className="row">
+                                <div>{item.title}</div>
+                                <div>{item.detail}</div>
+                            </div>
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
+    renderSummaryEvaluation = () => {
+        const dataSummaryEvaluation = [
+            {
+                'title': 'Thị giá',
+                'detail': '32,659.0 tỷ'
+            },
+            {
+                'title': 'EPS',
+                'detail': '4,623'
+            },
+            {
+                'title': 'P/E',
+                'detail': '10.42'
+            },
+            {
+                'title': 'P/S',
+                'detail': '1.14'
+            },
+            {
+                'title': 'P/B',
+                'detail': '2.11'
+            }
+        ]
+        return (
+            <div>
+                <List
+                    header={<div>ĐỊNH GIÁ</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={dataSummaryEvaluation}
+                    renderItem={item => (
+                        <List.Item>
+                            <div className="row">
+                                <div>{item.title}</div>
+                                <div>{item.detail}</div>
+                            </div>
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
+    renderSummarySupplyDemand = () => {
+        const dataSummarySupplyDemand = [
+            {
+                'title': 'Dư mua',
+                'detail': '2,748,930'
+            },
+            {
+                'title': 'Dư bán',
+                'detail': '2,974,430'
+            },
+            {
+                'title': 'KL Mua chủ động',
+                'detail': '1,570,780'
+            },
+            {
+                'title': 'KL Bán chủ động',
+                'detail': '1,348,360'
+            }
+        ]
+        return (
+            <div>
+                <List
+                    header={<div>CUNG CẦU</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={dataSummarySupplyDemand}
+                    renderItem={item => (
+                        <List.Item>
+                            <div className="row">
+                                <div>{item.title}</div>
+                                <div>{item.detail}</div>
+                            </div>
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
+    renderSummaryForeignInvestor = () => {
+        const dataSummaryForeignInvestor = [
+            {
+                'title': 'KL NN Mua',
+                'detail': '381,160'
+            },
+            {
+                'title': 'KL NN Bán',
+                'detail': '400,930'
+            },
+            {
+                'title': 'GT NN Mua	',
+                'detail': '19.59 tỷ'
+            },
+            {
+                'title': 'GT NN Bán',
+                'detail': '20.54 tỷ'
+            }
+        ]
+        return (
+            <div>
+                <List
+                    header={<div>GIAO DỊCH NĐTNN</div>}
+                    footer={<div>Footer</div>}
+                    bordered
+                    dataSource={dataSummaryForeignInvestor}
+                    renderItem={item => (
+                        <List.Item>
+                            <div className="row">
+                                <div>{item.title}</div>
+                                <div>{item.detail}</div>
+                            </div>
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
     render() {
-        return <div>Transaction
-            <ChartTV />
+        return <div className="Transaction">
+            <div className="Transaction-left-container">
+                <div className="Transaction-chartTV">
+                    {/* <ChartTV /> */}
+                    chartTV
+                </div>
+                <div className="Transaction-summary">
+                    <div className="row">
+                        {this.renderSummaryTransaction()}
+                        {this.renderSummaryEvaluation()}
+
+                    </div>
+                    <div className="row">
+                        {this.renderSummarySupplyDemand()}
+                        {this.renderSummaryForeignInvestor()}
+                    </div>
+                </div>
+            </div>
+            <div className="Transaction-right-container">
+                <div className="Trasaction-order-statistic">
+                    <List
+                        header={<div>Header</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={data}
+                        renderItem={item => (
+                            <List.Item>
+                                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+                            </List.Item>
+                        )}
+                    />
+                </div>
+                <div className="Trasaction-order-deal-statistic">
+                    <Tabs defaultActiveKey="1">
+                        <TabPane tab="Tab 1" key="1">
+                            Content of Tab Pane 1
+                        </TabPane>
+                        <TabPane tab="Tab 2" key="2">
+                            Content of Tab Pane 2
+                        </TabPane>
+                        <TabPane tab="Tab 3" key="3">
+                            Content of Tab Pane 3
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </div>
         </div>
     }
 }
